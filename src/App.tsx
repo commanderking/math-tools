913bb36ac6c3f54a92fc873d39ca16cfd10070d8
commanderking <yaoguais@Jeffreys-MacBook-Pro.local>
2020-05-30 15:50:15 -0400

    add absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import CoordinateGridContainer from "./components/coordinateGrid/CoordinateGridContainer";
+import CoordinateGridContainer from "components/coordinateGrid/CoordinateGridContainer";
 function App() {
   return (
     <div className="App">
