ff1498d405ed1ec002136b6b94ce6648ea0272ec
commanderking <yaoguais@Jeffreys-MacBook-Pro.local>
2020-05-30 16:12:48 -0400

    add setup for storybook

---
+++ src/components/coordinateGrid/CoordinateGridContainer.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CoordinateGridContainer from "./CoordinateGridContainer";
+
+export default {
+  component: CoordinateGridContainer,
+  title: "Coordinate Grid",
+};
+
+export const Basic = () => <CoordinateGridContainer id="BasicCoordinateGrid" />;
