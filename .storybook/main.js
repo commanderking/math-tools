a6c24f7f31c8434ec0dd2a307124b12637ec6bf2
Jeffrey King <jek582@mail.harvard.edu>
2020-08-15 09:22:09 -0400

    storybook upgrade to 6.0 and add docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ module.exports = {
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
-    "@storybook/addon-links",
+    "@storybook/addon-docs",
     "@storybook/preset-typescript",
     "@storybook/addon-knobs/register",
   ],
